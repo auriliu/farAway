2cdd60d1e299f2a9479db931b485a6db52959a9c
audesmus <aureliu.aurelius@gmail.com>
2023-11-16 14:10:23 +0100

    add all necessary components structure with dummy data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+//
+import "./index.css";
+
+function Logo() {
+  return <h1>far away</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>what do u need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">PackingList</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <p>Stats</p>
+    </footer>
+  );
+}
+
 function App() {
-  return <div className="App">far away starter</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 export default App;
