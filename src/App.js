09235f497535aa2fab3dfcce6ef2cf45de3a81b6
audesmus <aureliu.aurelius@gmail.com>
2023-11-16 14:28:35 +0100

    render packed items list using dummy data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 //
 import "./index.css";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
 function Logo() {
   return <h1>far away</h1>;
 }
@@ -12,24 +28,33 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">PackingList</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : null}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
       <p>Stats</p>
     </footer>
-  );
-}
-
-function App() {
-  return (
-    <div className="app">
-      <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
-    </div>
   );
 }
 
